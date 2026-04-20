a9115cf67bc12adb43d8ad38e349f763330dca09
seliv <aseliv@gmail.com>
2026-04-20 07:23:26 +0400

    feat: add Attachment entity and extend Message with reply/edit/delete fields

---
+++ backend/src/main/java/com/chatapp/repository/AttachmentRepository.java
@@ -0,0 +1,13 @@
+package com.chatapp.repository;
+
+import com.chatapp.entity.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AttachmentRepository extends JpaRepository<Attachment, UUID> {
+
+    List<Attachment> findByMessageId(UUID messageId);
+
+}
